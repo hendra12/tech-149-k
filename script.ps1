fb5f868d5b57293ee80e866f26b813aa42f4683e
hendra12 <h.gunawan72@yahoo.com>
2022-10-15 16:19:13 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -7,3 +7,4 @@ Temporary commit content for 2022-08-13T16:19:13
 Temporary commit content for 2022-09-02T16:19:13
 Temporary commit content for 2022-09-09T16:19:13
 Temporary commit content for 2022-10-05T16:19:13
+Temporary commit content for 2022-10-15T16:19:13
