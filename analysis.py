273314478f3538a999ffb08bbc7203abe56de11e
hendra12 <h.gunawan72@yahoo.com>
2022-12-19 16:19:13 +0100

    Added initial project plan

--- analysis.py
+++ analysis.py
@@ -14,3 +14,4 @@ Temporary commit content for 2022-11-21T16:19:13
 Temporary commit content for 2022-11-24T16:19:13
 Temporary commit content for 2022-11-25T16:19:13
 Temporary commit content for 2022-12-08T16:19:13
+Temporary commit content for 2022-12-19T16:19:13
