56e3070becdbc71c5b5f6c553abacf2daa7ccf0f
hendra12 <h.gunawan72@yahoo.com>
2023-07-13 16:19:13 +0100

    Added initial project plan

--- script.ps1
+++ script.ps1
@@ -28,3 +28,4 @@ Temporary commit content for 2023-05-25T16:19:13
 Temporary commit content for 2023-05-27T16:19:13
 Temporary commit content for 2023-07-01T16:19:13
 Temporary commit content for 2023-07-09T16:19:13
+Temporary commit content for 2023-07-13T16:19:13
