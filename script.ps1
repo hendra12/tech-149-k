388a343b6f9684b4ebf9b094b8d94d22ee2de9bb
hendra12 <h.gunawan72@yahoo.com>
2023-07-21 16:19:13 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -29,3 +29,4 @@ Temporary commit content for 2023-05-27T16:19:13
 Temporary commit content for 2023-07-01T16:19:13
 Temporary commit content for 2023-07-09T16:19:13
 Temporary commit content for 2023-07-13T16:19:13
+Temporary commit content for 2023-07-21T16:19:13
